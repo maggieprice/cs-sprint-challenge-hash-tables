b307c4abf5c985170f82469904c7fe4891ff357f
Justin Price <mr.price@Justins-iMac.local>
2020-06-05 15:19:35 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
+# import numpy as np
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    # empty hash
+    s = {}
+    # loop through array
+    for i, weight in enumerate(weights):
+        # subract an index value from hash
+        if limit - weight in s:
+            #if difference exists, return indices
+            return (i, s[limit - weight])
+            # give first index variable
+        s[weight] = i
+ 
     return None
